da629608888586df1027ed4759167f13f0420559
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-03 23:41:14 +0300

    Added properties parser

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by User on 03.03.2016.
+ */
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Properties properties = new Properties();
+        try (InputStream input = new FileInputStream("config.properties")) {
+            properties.load(input);
+
+            System.out.println(properties.getProperty("database"));
+            System.out.println(properties.getProperty("dbuser"));
+            System.out.println(properties.getProperty("dbpassword"));
+
+        } catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+}
