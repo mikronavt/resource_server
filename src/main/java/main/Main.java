f7286403358d710bd6a92eea8d96c71c4b9db1b3
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-04 15:14:49 +0300

    NIO example

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,6 +6,10 @@ import sax.ReadXMLFileSAX;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.ByteChannel;
+import java.nio.channels.FileChannel;
 import java.util.Properties;
 
 /**
@@ -13,6 +17,10 @@ import java.util.Properties;
  */
 public class Main {
     public static void main(String[] args) throws Exception{
+        /*
+        Testing properties
+
+         */
         Properties properties = new Properties();
         try (InputStream input = new FileInputStream("config.properties")) {
             properties.load(input);
@@ -25,8 +33,35 @@ public class Main {
             ex.printStackTrace();
         }
 
+        /*
+        Testing work with SAX
+         */
 
         DBParametersResource resource = (DBParametersResource) ReadXMLFileSAX.readXML("./data/MySqlResource.xdb");
         System.out.println(resource);
+
+        /*
+        testing NIO
+         */
+
+        RandomAccessFile aFile = new RandomAccessFile("data/data.txt", "rw");
+        FileChannel inChannel = aFile.getChannel();
+
+        ByteBuffer buf = ByteBuffer.allocate(64);
+
+        int bytesRead = inChannel.read(buf);
+        while (bytesRead!=-1){
+            System.out.println("Read " + bytesRead);
+            buf.flip();
+
+            while (buf.hasRemaining()){
+                System.out.println((char) buf.get());
+            }
+
+            System.out.println("\n");
+            buf.clear();
+            bytesRead = inChannel.read(buf);
+        }
+        aFile.close();
     }
 }
