61c690b776070ca41de875232d6ca23a2f7b2181
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-04 16:44:43 +0300

    Base of ResourceServerServlet

--- src/main/java/servlets/ResourceServerServlet.java
+++ src/main/java/servlets/ResourceServerServlet.java
@@ -1,7 +1,41 @@
 package servlets;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import resourceServer.ResourceServerI;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by User on 04.03.2016.
  */
-public class ResourceServerServlet {
+public class ResourceServerServlet extends HttpServlet {
+    static final Logger logger = LogManager.getLogger(ResourceServerServlet.class.getName());
+    public static final String PAGE_URL = "/resources";
+    private final ResourceServerI resourceServer;
+
+    public ResourceServerServlet(ResourceServerI resourceServer){
+        this.resourceServer = resourceServer;
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response){
+        response.setContentType("text/html;charset=utf-8");
+
+        String path = request.getParameter("path");
+
+        if(path != null){
+            //добавить функционал
+
+            response.setStatus(HttpServletResponse.SC_OK);
+            return;
+        } else {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        }
+
+
+
+    }
+
 }
