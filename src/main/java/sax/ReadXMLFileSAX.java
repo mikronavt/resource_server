1c8457bbda1029e9b4925cb4f25538382b8824a5
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-04 14:36:04 +0300

    ReadXMLFileSAX and SaxHandler classes

--- src/main/java/sax/ReadXMLFileSAX.java
+++ src/main/java/sax/ReadXMLFileSAX.java
@@ -1,7 +1,24 @@
 package sax;
 
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
 /**
  * Created by User on 04.03.2016.
  */
 public class ReadXMLFileSAX {
+    public static Object readXML(String xmlFile){
+        try{
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser parser = factory.newSAXParser();
+
+            SaxHandler handler = new SaxHandler();
+            parser.parse(xmlFile, handler);
+
+            return handler.getObject();
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
